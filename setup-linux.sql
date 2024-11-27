deda9027304c0da3f82ebf34c7211b55e50831a9
Matthew Whited <matt@whited.us>
2024-11-26 20:28:40 -0500

    update notes

--- setup-linux.sql
+++ setup-linux.sql
@@ -1,8 +1,11 @@
-CREATE DATABASE VectorTest
+CREATE DATABASE [VectorTest]
 GO
-USE VectorTest
+USE [VectorTest]
 GO
 
+EXEC sp_configure 'show advanced options', 1;
+RECONFIGURE;
+GO
 EXEC sp_configure 'clr strict security', 0;
 RECONFIGURE;
 GO
@@ -13,3 +16,35 @@ GO
 CREATE ASSEMBLY myVector
 FROM '/var/opt/mssql/bin/myVector.dll'
 WITH PERMISSION_SET = SAFE;
+GO
+
+CREATE TYPE Vector
+EXTERNAL NAME myVector.Vector;
+GO
+
+
+CREATE FUNCTION dbo.VECTOR_DISTANCE
+(
+    @distanceMetric NVARCHAR(20),
+    @vector1 Vector,
+    @vector2 Vector
+)
+RETURNS FLOAT
+AS EXTERNAL NAME myVector.Vector.VectorDistance;
+GO
+
+CREATE FUNCTION dbo.VECTOR_LENGTH
+(
+    @vector Vector
+)
+RETURNS INT
+AS EXTERNAL NAME myVector.Vector.VectorLength;
+GO
+
+CREATE FUNCTION dbo.VECTOR_MAGNITUDE
+(
+    @vector Vector
+)
+RETURNS FLOAT
+AS EXTERNAL NAME myVector.Vector.VectorMagnitude;
+GO
